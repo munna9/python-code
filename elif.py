3bf195bf2f1931a5e772004ee3b846292b601920
munna9 <9munna9@gmail.com>
2017-10-07 17:07:24 -0400

    change has done

---
+++ elif.py
@@ -0,0 +1,10 @@
+age = int(input ("what is your age:"))
+print
+if age<0:
+    print("you are under age")
+elif age == 13:
+    print("your are in teen age")
+elif age == 20 :
+    print("you are good to go")
+else:
+    print("you are done")
